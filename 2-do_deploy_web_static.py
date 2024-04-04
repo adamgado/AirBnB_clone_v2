caafbbd820644cadaf02e31847936ddba46725b8
adamgado <adamgado33@gmail.com>
2024-04-04 08:00:46 -0700

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,14 +8,14 @@ from fabric.api import run
 env.hosts = ["54.160.84.85", "54.157.163.209"]
 
 
-def do_deploy(arch_path):
+def do_deploy(archive_path):
     """distributes an archive to your web servers"""
-    if os.path.isfile(arch_path) is False:
+    if os.path.isfile(archive_path) is False:
         return False
     arch = archive_path.split("/")[-1]
     arch_name = arch.split(".")[0]
 
-    if put(arch_path, "/tmp/{}".format(arch)).failed is True:
+    if put(archive_path, "/tmp/{}".format(arch)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
            format(arch_name)).failed is True:
