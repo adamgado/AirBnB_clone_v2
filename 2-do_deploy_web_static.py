4d4913be4e901a14bc23b151fc1dcc30c7ab2a54
adamgado <adamgado33@gmail.com>
2024-04-04 08:18:03 -0700

    pycodestyle fix, line too long, arch_name shortened to name

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,30 +13,30 @@ def do_deploy(archive_path):
     if os.path.isfile(archive_path) is False:
         return False
     arch = archive_path.split("/")[-1]
-    arch_name = arch.split(".")[0]
+    name = arch.split(".")[0]
 
     if put(archive_path, "/tmp/{}".format(arch)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
-           format(arch_name)).failed is True:
+           format(name)).failed is True:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
-           format(arch_name)).failed is True:
+           format(name)).failed is True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(arch, arch_name)).failed is True:
+           format(arch, name)).failed is True:
         return False
     if run("rm /tmp/{}".format(arch)).failed is True:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(arch_name, arch_name)).failed is True:
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(arch_name)).failed is True:
+           format(name)).failed is True:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(arch_name)).failed is True:
+           format(name)).failed is True:
         return False
     return True
