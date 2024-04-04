402d43f29cf8d35db979bae581a0f0a039a811c2
adamgado <adamgado33@gmail.com>
2024-04-04 10:27:05 -0700

    100 fix?

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,20 +3,20 @@
 import os
 from fabric.api import *
 
-env.hosts = ['54.160.84.85', '54.157.163.209']
+env.hosts = ["54.160.84.85", "54.157.163.209"]
 
 
 def do_clean(number=0):
     """deletes out-of-date archives"""
     number = 1 if int(number) == 0 else int(number)
 
-    arch = sorted(os.listdir("versions"))
-    [arch.pop() for i in range(number)]
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(x)) for x in arch]
+        [local("rm ./{}".format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
-        arch = run("ls -tr").split()
-        arch = [x for x in arch if "web_static_" in x]
-        [arch.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(x)) for x in arch]
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
