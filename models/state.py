c4945922b1d4636fded9688d074551d03596c4f7
adamgado <adamgado33@gmail.com>
2024-04-19 17:01:06 +0000

    7 but not

--- models/state.py
+++ models/state.py
@@ -6,3 +6,13 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            c_list = []
+            for c in list(models.storage.all(C).values()):
+                if c.state_id == self.id:
+                    c_list.append(c)
+            return c_list
